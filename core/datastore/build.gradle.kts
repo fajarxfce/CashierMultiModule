ae574ab970e6c0625a06dfa72019b023b0a37e1a
fajarxfce <nurul.10120014@mahasiswa.unikom.ac.id>
2025-04-09 14:38:22 +0700

    feat(core): add datastore and model modules

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.library)
+    alias(libs.plugins.nowinandroid.android.library.jacoco)
+    alias(libs.plugins.nowinandroid.hilt)
+}
+
+android {
+    defaultConfig {
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+    namespace = "com.fajarxfce.core.datastore"
+}
+
+dependencies {
+    api(libs.androidx.dataStore)
+    api(projects.core.model)
+
+    implementation(projects.core.common)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+}
