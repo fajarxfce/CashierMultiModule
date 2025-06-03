bf25171f3c4339037ac58aaaf059491af17a14fc
fajarxfce <nurul.10120014@mahasiswa.unikom.ac.id>
2025-06-03 09:54:06 +0700

    feat(pos): add navigation bars padding to PosBottomBar layout

--- feature/pos/ui/src/main/java/com/fajarxfce/feature/pos/ui/component/PosBottomBar.kt
+++ feature/pos/ui/src/main/java/com/fajarxfce/feature/pos/ui/component/PosBottomBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -33,7 +34,8 @@ fun PosBottomBar(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(16.dp)
+                .navigationBarsPadding(),
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
